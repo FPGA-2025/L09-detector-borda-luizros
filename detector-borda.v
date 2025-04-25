0cf5afbaec8f9f28f18cb8dbe2b5a62b9815ed9a
Luiz Felipe <luiz.silva@smart.agr.br>
2025-04-25 06:00:20 -0300

    update l09

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,21 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
-    
-//insira seu código aqui
+
+    reg [1:0] entrada_delay;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            // Reset assíncrono: zera os registradores
+            entrada_delay <= 2'b00;
+            detector <= 2'b00;
+        end else begin
+            // Detecta borda de subida
+            detector <= (~entrada_delay & entrada);
+            entrada_delay <= entrada;
+        end
+    end
 
 endmodule
